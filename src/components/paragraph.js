306999ba3055afa3036f603ca3b158feafff52e6
wrsmall <william.r.small89@gmail.com>
2019-11-10 19:28:17 -0500

    sample

--- src/components/paragraph.js
+++ src/components/paragraph.js
@@ -7,7 +7,7 @@ function Paragraph() {
             This was the most difficult question you asked me. My quest into React is very young, and 
             it has quickly metamorphize into becoming my favorite language. I am aware that there is still much to learn 
             with React, but if I had to pick one thing to change, it would be having the ablity to import all components into
-            one global file, and be able to access them without having to import them indivdually into each componet file in which we use them.
+            one global file, and be able to access them without having to import them individually into each componet file in which we use them.
 </p>
     )
 }
