57407347d2c0a2bc96f5cd87b5af009f1d9738d2
wrsmall <william.r.small89@gmail.com>
2019-11-11 15:49:43 -0500

    Updates

--- precache-manifest.eac0ebc1ab56b16e3ab0540802421283.js
+++ precache-manifest.eac0ebc1ab56b16e3ab0540802421283.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fbd4ee9ca3c2704a90429436af49eaba",
+    "revision": "ed0c692dce8afe34faf08976ab0f5730",
     "url": "/sample/index.html"
   },
   {
-    "revision": "d7d33948ba3c20356f88",
+    "revision": "d2bd08a6e998cca3ea1d",
     "url": "/sample/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sample/static/js/2.0ce07895.chunk.js"
   },
   {
-    "revision": "d7d33948ba3c20356f88",
-    "url": "/sample/static/js/main.c8aedde4.chunk.js"
+    "revision": "d2bd08a6e998cca3ea1d",
+    "url": "/sample/static/js/main.a0733806.chunk.js"
   },
   {
     "revision": "13eccf5971d9a7cb2861",
